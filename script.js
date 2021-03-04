0b5885eed588e7cb6ab88e75d2cce94ff524881c
Ryan <ryanmahara@gmail.com>
2021-03-04 07:06:21 -0600

    added styling/look, testing dynamic add function

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+'use strict';
 
+const nozzle = {
+  diameter: 10,
+  type: 'FFSO',
+  clearance: 8,
+  partNumber: '704B10-08-TS',
+  orientation: 'std',
+  facing: 'external',
+};
+
+const test = document.createElement('div');
+
+for (const button of document.querySelectorAll('button')) {
+  button.addEventListener('click', function () {
+    console.log(
+      `Qty: 1     ${nozzle.partNumber}     ${nozzle.diameter}" #150 ${nozzle.type} Nozzle (${nozzle.clearance}" CL)`
+    );
+    document.body.appendChild(test);
+  });
+}
+
+//document.body.append(document.createElement('textarea'));
+//https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+
+//read the above
