ae52bc96305d7febf73642c148694c7ff98c6656
Ryan <ryanmahara@gmail.com>
2021-03-09 06:07:08 -0600

    fixed part list grid, buttons insert into grid,WIP

--- script.js
+++ script.js
@@ -14,24 +14,20 @@ const nozzles = {
   random: 'hello',
 };
 
-const test = document.createElement('div');
-const testcontent = document.createTextNode(
-  `Qty: 1     ${nozzle.partNumber}     ${nozzle.diameter}" #150 ${nozzle.type} Nozzle (${nozzle.clearance}" CL)`
-);
-test.appendChild(testcontent);
-
 function tableHTMLCreation() {
-  return `<div class = 'test3'>test injections</div> `;
+  return `<div class = 'item_1'>1</div>
+  <div class = 'item_2'>704T10-08-TS</div>
+  <div class = 'item_3'>3</div>
+  <div class = 'item_4'>10" #150 FFSO Nozzle (8" CL)</div>
+  <div class = 'item_5'>ET</div>`;
 }
 
 for (const button of document.querySelectorAll('button')) {
   button.addEventListener('click', function () {
-    console.log(
-      `Qty: 1     ${nozzle.partNumber}     ${nozzle.diameter}" #150 ${nozzle.type} Nozzle (${nozzle.clearance}" CL)`
-    );
-    document.body.appendChild(test);
     const newhtml = tableHTMLCreation();
-    document.querySelector('div').insertAdjacentHTML('afterend', newhtml);
+    document
+      .querySelector('.header_item_5')
+      .insertAdjacentHTML('afterend', newhtml);
   });
 }
 
@@ -39,3 +35,13 @@ for (const button of document.querySelectorAll('button')) {
 //https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
 
 //read the above
+// console.log(
+//   `Qty: 1     ${nozzle.partNumber}     ${nozzle.diameter}" #150 ${nozzle.type} Nozzle (${nozzle.clearance}" CL)`
+// );
+
+// const test = document.createElement('div');
+// const testcontent = document.createTextNode(
+//   `Qty: 1     ${nozzle.partNumber}     ${nozzle.diameter}" #150 ${nozzle.type} Nozzle (${nozzle.clearance}" CL)`
+// );
+// test.appendChild(testcontent);
+// document.body.appendChild(test);
