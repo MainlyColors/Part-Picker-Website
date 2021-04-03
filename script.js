d78c30010dbbfaca92745bacdeee3ab756afadb9
Ryan <ryanmahara@gmail.com>
2021-04-03 15:24:28 -0500

    toggle switch click event listeners added to switch ext to int

--- script.js
+++ script.js
@@ -33,10 +33,37 @@ for (const button of document.querySelectorAll('button')) {
   });
 }
 
-const beforeCheckboxElement = document.querySelector(
-  '.int_ext_checkbox_container'
-).firstChild;
-console.log(beforeCheckboxElement);
+const toggleSwitchExternal = document.querySelector(
+  '.int_ext_checkbox_container > p'
+);
+
+const toggleSwitchInternal = document.querySelector(
+  '.int_ext_checkbox_container > p:last-child'
+);
+
+const toggleSwtich = document.querySelector('.int_ext_checkbox');
+let ffsoNozzleHeading = document.querySelector('.heading_ffso');
+let rfsoNozzleHeading = document.querySelector('.heading_rfso');
+console.log(toggleSwitchExternal);
+console.log(toggleSwitchInternal);
+console.log(toggleSwtich);
+console.log(ffsoNozzleHeading);
+console.log(rfsoNozzleHeading);
+
+toggleSwtich.addEventListener('click', function () {
+  // turns paragraph tag on and off
+  toggleSwitchExternal.classList.toggle('toggle-on-JS');
+  toggleSwitchInternal.classList.toggle('toggle-on-JS');
+  // based on toggle switch, change h1 at top of nozzle grid
+  if (toggleSwitchExternal.classList.contains('toggle-on-JS')) {
+    ffsoNozzleHeading.textContent = 'External FFSO Nozzles';
+    rfsoNozzleHeading.textContent = 'External RFSO Nozzles';
+  } else {
+    ffsoNozzleHeading.textContent = 'Internal FFSO Nozzles';
+    rfsoNozzleHeading.textContent = 'Internal RFSO Nozzles';
+  }
+});
+
 //document.body.append(document.createElement('textarea'));
 //https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
 
